c4e61e712150f9bf7f5dce24ffb566f11c0e952e
DavidStewartLDN <davidstewart.work@gmail.com>
2020-02-25 10:45:44 +0000

    Create base structure and tidied up folder structure

--- app/app.rb
+++ app/app.rb
@@ -8,55 +8,13 @@ require './app/models/user'
 set :database, "sqlite3:fireworksbnb.sqlite3"
 
 class FireworksBnB < Sinatra::Base
+
   get '/' do
+  # this is how you bring in database models. this returns all lines in each table for each class and saves it as an instance variable
     @users = User.all
     @spaces = Space.all
     @bookings = Booking.all
     erb :index
   end
 
-  # login to users account
-
-  get '/login' do
-    erb :login
-  end
-
-  # Verify login details against database
-
-  post '/login/validate' do
-    # erb :
-  end
-
-  # Add new user
-
-  get '/add_user' do
-    erb :add_user
-  end
-
-  post '/add_user/new' do
-    new_user = User.create(username: params[:new_user_name], password: params[:new_password])
-    redirect '/'
-  end
-
-  # Add new Space
-
-  get '/add_space' do
-    erb :add_space
-  end
-
-  post '/add_space/new' do
-    new_space = Space.create(property_name: params[:new_space_name], description: params[:new_space_description], price: params[:new_price])
-    redirect '/'
-  end 
-
-  # add new Booking
-
-  get '/add_booking' do
-    erb :add_booking
-  end
-
-  post '/add_booking/new' do
-    new_booking = Booking.create(start_date: params[:new_start_date], end_date: params[:new_end_date], confirmation: 'Pending')
-    redirect '/'
-  end 
 end
