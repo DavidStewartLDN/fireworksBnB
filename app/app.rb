e427e72eabf6f45c2466056ef004df2584b10a32
Mrtly <mrtly.ca@gmail.com>
2020-02-25 14:20:06 +0000

    rspec setup with correct requirements and 1st test puts users in db

--- app/app.rb
+++ app/app.rb
@@ -1,20 +1,18 @@
-require 'sinatra'
-require 'sinatra/activerecord'
+require "sinatra"
+require "sinatra/activerecord"
 
-require './app/models/booking'
-require './app/models/space'
-require './app/models/user'
+require_relative "./models/booking"
+require_relative "./models/space"
+require_relative "./models/user"
 
 set :database, "sqlite3:fireworksbnb.sqlite3"
 
 class FireworksBnB < Sinatra::Base
-
-  get '/' do
-  # this is how you bring in database models. this returns all lines in each table for each class and saves it as an instance variable
+  get "/" do
+    # this is how you bring in database models. this returns all lines in each table for each class and saves it as an instance variable
     @users = User.all
     @spaces = Space.all
     @bookings = Booking.all
     erb :index
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
   has_many :spaces
   has_many :bookings
+
+  attr_reader :username, :password
+
+  # this creates a new user successfuly with username and password
+  # at the moment it passes it to the main db
+  def self.sign_up(username:, password:)
+    @username = username
+    @password = password
+    self.create(username: username, password: password)
+  end
 end
