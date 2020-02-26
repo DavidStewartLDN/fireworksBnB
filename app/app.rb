42957bfaa4aeb2560dda6c0536cc10d7cfd6df90
hansa-lallu <hansalallu@gmail.com>
2020-02-26 11:12:53 +0000

    Has a sign up page

--- app/app.rb
+++ app/app.rb
@@ -28,6 +28,10 @@ class FireworksBnB < Sinatra::Base
     erb :login
   end
 
+  get '/sign_up' do
+    erb :sign_up
+  end
+
   # # Verify login details against database
 
   # post '/login/validate' do
