47e8ba47b7ba27f9d72de8788a8319053cfd651d
DavidStewartLDN <davidstewart.work@gmail.com>
2020-02-26 22:57:10 +0000

    removed foreign keys from tbale migration

--- db/migrate/20200224155546_create_tables.rb
+++ db/migrate/20200224155546_create_tables.rb
@@ -9,15 +9,12 @@ class CreateTables < ActiveRecord::Migration[6.0]
       t.string :property_name
       t.text :description
       t.integer :price
-      t.belongs_to :user, foreign_key: true
     end
 
     create_table :bookings do |t|
       t.datetime :start_date
       t.datetime :end_date
       t.string :confirmation
-      t.belongs_to :space, foreign_key: true
-      t.belongs_to :user, foreign_key: true
     end
 
   end
