64d89d78e7fa7f7ff146877bb23c07f1764e56b4
DavidStewartLDN <davidstewart.work@gmail.com>
2020-02-26 17:58:42 +0000

    add foreign keys to table, other changed files can be discarded

--- app/app.rb
+++ app/app.rb
@@ -5,7 +5,7 @@ require_relative "./models/booking"
 require_relative "./models/space"
 require_relative "./models/user"
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'fireworksbnb')
+# ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'fireworksbnb')
 
 class FireworksBnB < Sinatra::Base
   get "/" do
