815332b78337b1155957aefbddc8834d23d54bec
hansa-lallu <hansalallu@gmail.com>
2020-02-26 15:44:04 +0000

    Refactor of everything with sign up adding user to database

--- app/app.rb
+++ app/app.rb
@@ -34,39 +34,10 @@ class FireworksBnB < Sinatra::Base
     erb :login
   end
 
-  get 'add_user' do
-    erb :add_user
-  end
-
   post '/add_user/new' do
     session[:user] = params["new_user_name"]
     session[:password] = params["new_password"]
-    p params["new_user_name"]
-    p params["new_password"]
-    if (User.exists?(username: params["new_user_name"], password: params["new_password"]))
-      session[:user_id] = User.find_by(username: params["username"]).id
-      session[:user] = params["username"]
-      redirect '/'
-    else
-      new_user = User.create(username: params["new_user_name"], password: params["new_password"])
-      redirect '/login'
-    end
-    redirect '/login'
-  end
-
-
-  post '/login/validate' do
-    session[:user_entered] = params["username"]
-    session[:password_entered] = params["password"]
-    
-
-    if (User.exists?(username: params["username"], password: params["password"]))
-          session[:user_id] = User.find_by(username: params["username"]).id
-          session[:user] = params["username"]
-          redirect '/'
-        else
-          redirect '/login'
-        end
+    new_user = User.create(username: params["new_user_name"], password: params["new_password"])
     redirect '/login'
   end
 
@@ -74,43 +45,4 @@ class FireworksBnB < Sinatra::Base
     erb :sign_up
   end
 
-  # # Verify login details against database
-
-  # post '/login/validate' do
-  #   # session[:user_entered] = params["username"]
-  #   # session[:password_entered] = params["password"]
-  #   # redirect '/list_all'
-
-  #   if (User.exists?(username: params["username"], password: params["password"]))
-  #     session[:user_id] = User.find_by(username: params["username"]).id
-  #     session[:user] = params["username"]
-  #     redirect '/list_all'
-  #   else
-  #     redirect '/login'
-  #   end
-  # end
-
-  # # Main page listing all users, spaces and bookings
-
-  # get '/list_all' do
-  #   # @user_entered = session[:user_entered]
-  #   # @password_entered = session[:password_entered]
-  #   @current_user = session[:user]
-  #   @user_id = session[:user_id]
-  #   @users = User.all
-  #   @spaces = Space.all
-  #   @bookings = Booking.all
-  #   erb :list_all
-  # end
-
-  # # Add new user
-
-  # get '/add_user' do
-  #   erb :add_user
-  # end
-
-  # post '/add_user/new' do
-   
-  #   redirect '/list_all'
-  # end  
 end
