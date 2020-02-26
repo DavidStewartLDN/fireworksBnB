d186d9a50096c911191dda150686f7b506a502a9
hansa-lallu <hansalallu@gmail.com>
2020-02-26 15:31:00 +0000

    User can sign up

--- app/app.rb
+++ app/app.rb
@@ -5,20 +5,26 @@ require_relative "./models/booking"
 require_relative "./models/space"
 require_relative "./models/user"
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'fireworksbnb')
+
+# DB_ENV ='development'
+# connection_details =YAML::load(File.open('./config/database.yml'))
+# ActiveRecord::Base.establish_connection(connection_details[DB_ENV])
+
+# set :database, "sqlite3:fireworksbnb.sqlite3"
 
 class FireworksBnB < Sinatra::Base
-  # get "/" do
+
+
   #   # this is how you bring in database models. this returns all lines in each table for each class and saves it as an instance variable
+  enable :sessions
+
+  # home page 
+
+  get '/' do
     @users = User.all
     @spaces = Space.all
     @bookings = Booking.all
-  #   erb :list_all
-  # end
-
-  enable :sessions
-
-  get '/' do
+    @testing_user = session[:user]
     erb :index
   end
 
@@ -28,16 +34,44 @@ class FireworksBnB < Sinatra::Base
     erb :login
   end
 
-  get '/sign_up' do
-    erb :sign_up
+  get 'add_user' do
+    erb :add_user
   end
+
+  post '/add_user/new' do
+    session[:user] = params["new_user_name"]
+    session[:password] = params["new_password"]
+    p params["new_user_name"]
+    p params["new_password"]
+    if (User.exists?(username: params["new_user_name"], password: params["new_password"]))
+      session[:user_id] = User.find_by(username: params["username"]).id
+      session[:user] = params["username"]
+      redirect '/'
+    else
+      new_user = User.create(username: params["new_user_name"], password: params["new_password"])
+      redirect '/login'
+    end
+    redirect '/login'
+  end
+
 
   post '/login/validate' do
     session[:user_entered] = params["username"]
     session[:password_entered] = params["password"]
-    new_user = User.create(username: params[:new_user_name], password: params[:new_password])
-    erb :login
-    # redirect '/list_all'
+    
+
+    if (User.exists?(username: params["username"], password: params["password"]))
+          session[:user_id] = User.find_by(username: params["username"]).id
+          session[:user] = params["username"]
+          redirect '/'
+        else
+          redirect '/login'
+        end
+    redirect '/login'
+  end
+
+  get '/sign_up' do
+    erb :sign_up
   end
 
   # # Verify login details against database
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -2,8 +2,8 @@ feature 'user is able to sign up' do
   scenario 'User can enter a name' do
     visit('/')
     click_link('Sign Up')
-    fill_in 'username', with: 'test@test.com'
-    fill_in 'password', with: 'potato'
+    fill_in 'new_user_name', with: 'test@test.com'
+    fill_in 'new_password', with: 'potato'
     click_button('Sign up')
     expect(page).to have_content('Login')
     expect(page).to have_content('Please enter your account details')
