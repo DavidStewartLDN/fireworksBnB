12dcdc9eef436d2a3450cf812d544c878c0ca6f4
DavidStewartLDN <davidstewart.work@gmail.com>
2020-02-28 11:13:11 +0000

    Correct my_spaces_spec to take into accoutn new date calendar date format

--- spec/features/my_spaces_spec.rb
+++ spec/features/my_spaces_spec.rb
@@ -17,7 +17,7 @@ feature 'user is able to see their spaces' do
 
   end
 
-  scenario 'this' do
+  scenario 'user can see current pending rquests for their property with dates' do
 
     User.create(username: 'Owner', password: 'potato')
     User.create(username: 'Traveller', password: 'potato')
@@ -48,8 +48,8 @@ feature 'user is able to see their spaces' do
 
     click_button('Book Lovely old house')
 
-    fill_in 'start_date', with: '27/02/2020'
-    fill_in 'end_date', with: '28/02/2020'
+    fill_in 'start_date', with: '2020-02-27'
+    fill_in 'end_date', with: '2020-02-28'
     click_button('Request Dates')
 
     # Owner logs in and views 
@@ -64,6 +64,8 @@ feature 'user is able to see their spaces' do
 
     expect(page).to have_content('My current listings')
     expect(page).to have_content('Lovely old house')
+    expect(page).to have_content('2020-02-27')
+    expect(page).to have_content('2020-02-28')
   
   end
 end
