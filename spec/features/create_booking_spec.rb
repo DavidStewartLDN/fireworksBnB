174a59f07a3329653a44ca3de05de02e658698ae
hansa-lallu <hansalallu@gmail.com>
2020-02-28 10:35:22 +0000

    web helper added

--- spec/features/create_booking_spec.rb
+++ spec/features/create_booking_spec.rb
@@ -8,3 +8,12 @@ feature 'user is able to book a space' do
     click_button("Book Lovely old house")
     expect(page).to have_content("Request Booking")
   end
+
+  scenario 'user gets confirmation a request was made' do
+    users_log_in_list_space_request_booking
+    expect(page).to have_content("Request sent")
+    expect(page).to have_content("Your current bookings for this property are")
+    expect(page).to have_content("2020-02-27")
+    expect(page).to have_content("2020-02-28")
+  end
+end
